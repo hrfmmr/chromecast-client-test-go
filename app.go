9b4d57913b5932c68d8d1b46ee516348e7b57f70
hrfmmr <i@hrfmmr.com>
2021-06-02 09:42:59 +0900

    Add support for establishing tcp w/ tls socket

--- app.go
+++ app.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+const (
+	googlehomeIP          = "192.168.11.4"
+	chromecastDefaultPort = 8009
+)
+
+func connect(addr string, port int) {
+	dialer := &net.Dialer{
+		Timeout: time.Second * 15,
+	}
+	_, err := tls.DialWithDialer(dialer, "tcp", fmt.Sprintf("%s:%d", addr, port), &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("✅ connected!")
+}
 
 func main() {
-	fmt.Println("hello, world")
+	connect(googlehomeIP, chromecastDefaultPort)
 }
